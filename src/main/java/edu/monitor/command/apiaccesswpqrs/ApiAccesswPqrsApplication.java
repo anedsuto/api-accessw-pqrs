cb516fda2b98c2bac7b4a009fa5526fb1a367a27
Angela EST <anedsuto@gmail.com>
2023-06-03 19:50:31 +0200

    Initial commit

---
+++ src/main/java/edu/monitor/command/apiaccesswpqrs/ApiAccesswPqrsApplication.java
@@ -0,0 +1,13 @@
+package edu.monitor.command.apiaccesswpqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccesswPqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAccesswPqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/monitor/command/apiaccesswpqrs/ApiAccesswPqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.monitor.command.apiaccesswpqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccesswPqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
